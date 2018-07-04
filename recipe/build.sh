8b45de03855bcf00a5f477e50117c12f40b17ad0
Filipe Fernandes <ocefpaf@gmail.com>
2018-07-04 13:51:17 -0300

    strip

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,10 @@ set -e # Abort on error.
 # Force python bindings to not be built.
 unset PYTHON
 
+if [ $(uname) != Darwin ]; then
+  export CFLAGS="-O2 -Wl,-S $CFLAGS"
+  export CXXFLAGS="-O2 -Wl,-S $CXXFLAGS"
+fi
 export CPPFLAGS="$CPPFLAGS -I$PREFIX/include"
 
 # Filter out -std=.* from CXXFLAGS as it disrupts checks for C++ language levels.
